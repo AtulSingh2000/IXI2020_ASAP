77bc3b8a92a05d43e3f0eac727917de308a3ee85
Prabhakar Deep <thatguypdeep@gmail.com>
2020-02-05 04:00:01 +0530

    2nd evaluation

---
+++ voiceCmd/Assets/avatarControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class avatarControl : MonoBehaviour
+{
+
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private string recognizedText;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            if (recognizedText == "Jump")
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+}
